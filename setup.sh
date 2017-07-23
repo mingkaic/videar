1f257a0c2dd4d32af6b2cdd27b95f7b94e359fc3
mingkaic <mingkaichen2009@gmail.com>
2017-07-22 20:32:55 -0700

    add complete setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
 apt-get update && apt-get -y dist-upgrade
+apt-get install build-essential
+
+# install nodejs
+apt-get install -y python-software-properties
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+
+# install npm dependencies
+npm i
+npm i -g @angular/cli@1.0.0
+
+# install ffmpeg
 apt-get install -y ffmpeg
